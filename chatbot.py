ff9a6a3a8c2f6fda099883f358b9c3a8d6cd7eca
dchung <do.chung1209@gmail.com>
2024-09-05 00:51:44 -0400

    Updated an Engine

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,44 @@
-print("start...")
-
 import streamlit as st
+import os
+import nest_asyncio
+from llama_parse import LlamaParse
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from io import BytesIO
+from tempfile import NamedTemporaryFile
 
-st.title('Emotional Chat Bot')
+nest_asyncio.apply()
 
-if "counter" not in st.session_state:
-    st.session_state.counter = 0
 
-prompt = st.chat_input('Enter your message here:')
-if prompt:
-    with st.chat_message("You"):
-        st.write(prompt)
-    
-    with st.spinner('Processing...'):
-        st.session_state.counter += 1
-    
-    with st.chat_message("Bot"):
-        st.write(f"Hello! You have sent {st.session_state.counter} messages so far.")
+os.environ["OPENAI_API_KEY"] = ""
+os.environ["LLAMA_CLOUD_API_KEY"] = ""
 
-print("end...")
+parser = LlamaParse(
+    use_vendor_multimodal_model=True,
+    vendor_multimodal_model_name="openai-gpt4o",
+    vendor_multimodal_api_key=os.environ["OPENAI_API_KEY"],
+    num_workers=4,
+    language="en"
+)
+
+
+def main():
+    st.title('PDF ChatBot by dchung')
+    uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
+    if uploaded_file is not None:
+        with NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+            tmp_file.write(uploaded_file.read())
+        
+        documents = parser.load_data(tmp_file.name)
+        st.write("Processing...")
+        print(documents)
+        os.remove(tmp_file.name)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
