748ed64492caf07651d23bd62c7d4b3f61870e2e
dchung <do.chung1209@gmail.com>
2024-09-06 00:15:00 -0400

    Updated

--- chatbot.py
+++ chatbot.py
@@ -3,35 +3,63 @@ import os
 import nest_asyncio
 from llama_parse import LlamaParse
 from llama_index.llms.openai import OpenAI
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, Settings
+from langchain.text_splitter import MarkdownTextSplitter
+from langchain_openai import OpenAIEmbeddings
+
+
 from io import BytesIO
 from tempfile import NamedTemporaryFile
 
 nest_asyncio.apply()
 
+def set_os_env(openai_api_key, llama_api_key):
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+    os.environ["LLAMA_CLOUD_API_KEY"] = llama_api_key
 
-os.environ["OPENAI_API_KEY"] = ""
+os.environ["OPENAI_API_KEY"] =""
 os.environ["LLAMA_CLOUD_API_KEY"] = ""
 
-parser = LlamaParse(
-    use_vendor_multimodal_model=True,
-    vendor_multimodal_model_name="openai-gpt4o",
-    vendor_multimodal_api_key=os.environ["OPENAI_API_KEY"],
-    num_workers=4,
-    language="en"
-)
+Settings.llm = 
+Settings.tokenizer = 
+Settings.embed_model = OpenAIEmbeddings(
+        api_key=os.environ["OPENAI_API_KEY"],
+        model = "text-embedding-3-small")
+
+def pdf_to_md(file, llama_api_key):
+    parser = LlamaParse(
+        api_key = llama_api_key,
+        result_type = "markdown",
+        num_workers=4,
+        language="en"
+    )
+    return parser.load_data(file)
+
+def embeded_text(documents):
+    #Chunking
+    markdown_splitter = MarkdownTextSplitter(chunk_size=256, chunk_overlap=0)
+    docs = markdown_splitter.create_documents(documents)
+    embeddings_model = OpenAIEmbeddings(
+        api_key=os.environ["OPENAI_API_KEY"],
+        model = "text-embedding-3-small")
+    embeddings = embeddings_model.embed(docs)
+    return embeddings
+
 
 
 def main():
     st.title('PDF ChatBot by dchung')
+    """openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+    llama_api_key = st.sidebar.text_input("Llama API Key", type="password")"""
+
     uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
     if uploaded_file is not None:
         with NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
             tmp_file.write(uploaded_file.read())
         
-        documents = parser.load_data(tmp_file.name)
-        st.write("Processing...")
-        print(documents)
+        documents = pdf_to_md(tmp_file.name, os.environ["LLAMA_CLOUD_API_KEY"])
+        st.write("Successfully procsessed✅")
+        embeded_text(documents)
         os.remove(tmp_file.name)
 
 
